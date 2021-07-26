d9975e25af127cc6c2c639c62b3cd0bd56c05892
Enzo David Dure Barua <edure@RIEDER.COM>
2021-07-26 17:25:56 -0400

    Añadida funcionalidad de agregar tareas y diseño

--- src/components/TareaApp.vue
+++ src/components/TareaApp.vue
@@ -1,15 +1,24 @@
 <template>
     <h1>App Tareas</h1>
     <tarea-form />
+    <tarea-item 
+        v-for="tarea in tareas"
+        :key="tarea.id"
+        :tarea="tarea"
+    />
+    <div class="alert alert-dark mt-3" v-if="tareas.length === 0">
+        Sin tareas pendientes 😄
+    </div>
     {{ tareas }}
 </template>
 <script>
 import { ref } from '@vue/reactivity'
 import TareaForm from './TareaForm.vue';
 import { provide } from '@vue/runtime-core';
+import TareaItem from './TareaItem.vue';
 
 export default {
-  components: { TareaForm },
+  components: { TareaForm, TareaItem },
     setup(){
         const tareas = ref([]);
 
--- src/components/TareaItem.vue
+++ src/components/TareaItem.vue
@@ -1,9 +1,17 @@
 <template>
-    <h2>Child</h2>
+    <div class="alert alert-warning mt-3 d-flex justify-content-between align-items-center">
+        <p class="m-0">{{ tarea.texto }}</p>
+        <div>
+            <i class="fas fa-check-circle mx-2 text-success" role="button"></i>
+            <i class="fas fa-minus-circle text-danger" role="button"></i>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-    
+    props: {
+        tarea: Object
+    }
 }
 </script>
